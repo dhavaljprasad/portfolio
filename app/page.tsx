767c0c41f9cfac873fd95e69918523de76cf4ade
dhavaljprasad <dhavaljprasad@gmail.com>
2026-04-17 08:45:40 +0530

    fix: fixed app's hero section and landing page's max width for render

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import { ProfileHero } from "@/components/custom/profile-hero";
 export default function Page() {
   return (
     <div className="w-full min-h-screen flex items-start justify-center bg-background-primary scroll-y-auto">
-      <div className="w-full flex flex-col gap-4 p-4 md:w-4xl md:py-8">
+      <div className="w-full flex flex-col gap-4 p-4 sm:w-xl md:w-4xl md:py-8">
         <ProfileHero />
       </div>
     </div>
--- components/custom/profile-hero.tsx
+++ components/custom/profile-hero.tsx
@@ -3,35 +3,35 @@
 import { useEffect, useState } from "react";
 import { PulsatingButton } from "./button";
 import { FiGithub } from "react-icons/fi";
-import { FaXTwitter, FaLinkedinIn } from "react-icons/fa6";
+import { FaXTwitter, FaLinkedinIn, FaInstagram } from "react-icons/fa6";
 
-const BannerImageData = [
+const socialsData = [
   {
-    type: "image",
-    url: "https://res.cloudinary.com/dli14hm5i/image/upload/v1773622185/Personal%20Projects%20Dev/New%20Portfolio%20Website/3b4398b13744e2ac3cf4d267778e0b2d_esjkbs_bb8665.jpg",
+    name: "Instagram",
+    icon: FaInstagram,
+    url: "https://www.instagram.com/dhavaljprasad/",
   },
   {
-    type: "image",
-    url: "https://res.cloudinary.com/dli14hm5i/image/upload/v1773623644/87199dfe3f83fbf0d5a31f138bdec20d_w9ci10.jpg",
+    name: "GitHub",
+    icon: FiGithub,
+    url: "https://github.com/dhavaljprasad",
   },
   {
-    type: "image",
-    url: "https://res.cloudinary.com/dli14hm5i/image/upload/v1773623748/74fcb62ce2b570b82d0c1f55705c0ad2_waxx6x.jpg",
+    name: "X (Twitter)",
+    icon: FaXTwitter,
+    url: "https://x.com/dhavaljprasad",
   },
   {
-    type: "image",
-    url: "https://res.cloudinary.com/dli14hm5i/image/upload/v1773623644/27a82053e12cee498604ac12d23a96ae_hu4bly.jpg",
-  },
-  {
-    type: "image",
-    url: "https://res.cloudinary.com/dli14hm5i/image/upload/v1773623699/0757fb7051f0b7f9877217f83433cf0a_syo3vx.jpg",
+    name: "LinkedIn",
+    icon: FaLinkedinIn,
+    url: "https://www.linkedin.com/in/dhavaljprasad/",
   },
 ];
 
 export const ProfileHero = () => {
-  const [time, setTime] = useState(getElapsedTime());
+  const [time, setTime] = useState("0y, 0m, 0d, 00:00:00");
 
-  function getElapsedTime(): string {
+  const getElapsedTime = (): string => {
     const now = new Date();
     const START_DATE = new Date("2004-08-04T00:00:00");
     const diff = now.getTime() - START_DATE.getTime();
@@ -54,61 +54,69 @@ export const ProfileHero = () => {
       2,
       "0",
     )}:${String(minutes).padStart(2, "0")}:${String(seconds).padStart(2, "0")}`;
-  }
+  };
 
   useEffect(() => {
     const interval = setInterval(() => {
       setTime(getElapsedTime());
-    }, 50);
+    }, 1000);
 
     return () => clearInterval(interval);
   }, []);
 
   return (
-    <div className="w-full h-fit flex flex-col items-center justify-center gap-2 rounded-sm relative">
-      <img
-        src={
-          "https://res.cloudinary.com/dli14hm5i/image/upload/v1773622185/Personal%20Projects%20Dev/New%20Portfolio%20Website/3b4398b13744e2ac3cf4d267778e0b2d_esjkbs_bb8665.jpg"
-        }
-        alt="banner header image"
-        className="aspect-[90/15] object-cover rounded-sm"
-      />
-      <div className="w-full flex items-center justify-between absolute top-[15vh] px-10">
-        <div className="w-auto h-auto flex items-center justify-center gap-2">
-          <div className="w-30 h-30 border border-4 border-background-primary rounded-sm">
-            <img
-              src={
-                "https://res.cloudinary.com/dli14hm5i/image/upload/v1773624270/ChatGPT_Image_Jan_15_2026_07_37_32_PM_abe9ai.png"
-              }
-              alt="dhaval's pfp"
-              className="w-full w-full"
-            />
-          </div>
-          <div className="w-auto flex flex-col gap-1 mt-10">
-            <h1 className="text-4xl font-bold">Dhaval J Prasad</h1>
-            <span className="text-sm text-text-secondary">{time}</span>
-          </div>
-        </div>
-        <div className="w-auto h-auto flex flex-col items-end justify-center gap-2 mt-10">
-          <PulsatingButton text="Hire Me Now" onClick={() => {}} />
-          <div className="flex items-center justify-center gap-2">
-            <FiGithub
-              className="text-background-ternary hover:text-white cursor-pointer"
-              size={22}
-            />
-            <FaXTwitter
-              className="text-background-ternary hover:text-white cursor-pointer"
-              size={22}
-            />
-            <FaLinkedinIn
-              className="text-background-ternary hover:text-white cursor-pointer"
-              size={22}
-            />
-          </div>
-        </div>
+    <div className="w-full h-fit flex flex-col justify-center gap-2 rounded-sm relative">
+      {/* Video Banner */}
+      <div className="w-full aspect-[2/1] sm:aspect-[4/1] md:aspect-[90/15] overflow-hidden rounded-sm">
+        <video
+          autoPlay
+          loop
+          muted
+          className="w-full h-full object-cover object-right md:object-center"
+          src={
+            "https://res.cloudinary.com/dli14hm5i/video/upload/q_auto/f_auto/v1776383053/Portfolio_Banner_Video_r9otwn.mp4"
+          }
+        ></video>
       </div>
-      <div className="mt-24">
-        <p className="text-text-secondary text-justify">
+
+      {/* pfp image, name, age, socials, hire me button */}
+      <div className="w-full h-fit flex flex-col gap-4 items-center justify-center absolute sm:static md:absolute px-0 sm:px-4 md:px-10 top-[15vh] md:top-[15vh]">
+        <div className="w-full h-fit flex flex-col sm:flex-row gap-2 sm:gap-0 sm:flex-row items-center justify-between">
+          <div className="w-fit h-auto flex flex-col sm:flex-row items-center justify-start gap-2">
+            <div className="w-35 h-35 sm:w-20 sm:h-20 md:w-30 md:h-30 border border-4 border-background-primary rounded-sm">
+              <img
+                src="https://res.cloudinary.com/dli14hm5i/image/upload/v1773624270/ChatGPT_Image_Jan_15_2026_07_37_32_PM_abe9ai.png"
+                alt="dhaval's pfp"
+                className="w-full h-full object-cover rounded-sm"
+              />
+            </div>
+
+            <div className="w-auto flex flex-col items-center sm:items-start gap-1 md:mt-10">
+              <h1 className="text-2xl sm:text-2xl md:text-4xl font-bold">
+                Dhaval J Prasad
+              </h1>
+              <span className="text-sm text-text-secondary">{time}</span>
+            </div>
+          </div>
+
+          <div className="w-fit h-auto flex flex-col items-center sm:items-end justify-center gap-2 md:mt-10">
+            <PulsatingButton text="Hire Me Now" onClick={() => {}} />
+
+            <div className="flex items-center justify-center gap-2">
+              {socialsData.map((social, index) => {
+                return (
+                  <social.icon
+                    size={22}
+                    key={index}
+                    // onClick={()=>}
+                    className="text-background-ternary hover:text-white cursor-pointer"
+                  />
+                );
+              })}
+            </div>
+          </div>
+        </div>
+        <p className="text-text-secondary text-justify text-base sm:text-sm md:text-base">
           <span className="text-text-primary">Senior Software Engineer</span>{" "}
           building production-grade web platforms, AI-powered products, and
           scalable systems. Experienced in Next.js, React, FastAPI, and
@@ -117,6 +125,18 @@ export const ProfileHero = () => {
           data-heavy dashboards and automation pipelines.
         </p>
       </div>
+
+      {/* extensive description */}
+      {/* <div className="">
+        <p className="text-text-secondary text-justify text-base sm:text-sm md:text-base">
+          <span className="text-text-primary">Senior Software Engineer</span>{" "}
+          building production-grade web platforms, AI-powered products, and
+          scalable systems. Experienced in Next.js, React, FastAPI, and
+          distributed backend architectures. I focus on clean UI, performance,
+          and shipping real products—from GenAI agents and RAG systems to
+          data-heavy dashboards and automation pipelines.
+        </p>
+      </div> */}
     </div>
   );
 };
